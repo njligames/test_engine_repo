a81878764226271f6cff67f7124857be58408380
njligames <jamesfolk1@gmail.com>
2018-04-05 10:33:56 -0400

    added the beginnings of google vr

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,306 +1,157 @@
-/*
-  Copyright (C) 1997-2017 Sam Lantinga <slouken@libsdl.org>
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely.
-*/
-
-/* Simple program:  draw as many random objects on the screen as possible */
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-
-#ifdef __EMSCRIPTEN__
-#include <emscripten/emscripten.h>
-#endif
-
-#include "SDL_test_common.h"
-
-#define NUM_OBJECTS 100
-
-static SDLTest_CommonState *state;
-static int num_objects;
-static SDL_bool cycle_color;
-static SDL_bool cycle_alpha;
-static int cycle_direction = 1;
-static int current_alpha = 255;
-static int current_color = 255;
-static SDL_BlendMode blendMode = SDL_BLENDMODE_NONE;
-
-int done;
-
-void
-DrawPoints(SDL_Renderer * renderer)
+#include "Engine.h"
+int main(int argc, char *argv[])
 {
-    int i;
-    int x, y;
-    SDL_Rect viewport;
-
-    /* Query the sizes */
-    SDL_RenderGetViewport(renderer, &viewport);
-
-    for (i = 0; i < num_objects * 4; ++i) {
-        /* Cycle the color and alpha, if desired */
-        if (cycle_color) {
-            current_color += cycle_direction;
-            if (current_color < 0) {
-                current_color = 0;
-                cycle_direction = -cycle_direction;
-            }
-            if (current_color > 255) {
-                current_color = 255;
-                cycle_direction = -cycle_direction;
-            }
-        }
-        if (cycle_alpha) {
-            current_alpha += cycle_direction;
-            if (current_alpha < 0) {
-                current_alpha = 0;
-                cycle_direction = -cycle_direction;
-            }
-            if (current_alpha > 255) {
-                current_alpha = 255;
-                cycle_direction = -cycle_direction;
-            }
-        }
-        SDL_SetRenderDrawColor(renderer, 255, (Uint8) current_color,
-                               (Uint8) current_color, (Uint8) current_alpha);
-
-        x = rand() % viewport.w;
-        y = rand() % viewport.h;
-        SDL_RenderDrawPoint(renderer, x, y);
-    }
-}
-
-void
-DrawLines(SDL_Renderer * renderer)
-{
-    int i;
-    int x1, y1, x2, y2;
-    SDL_Rect viewport;
-
-    /* Query the sizes */
-    SDL_RenderGetViewport(renderer, &viewport);
-
-    for (i = 0; i < num_objects; ++i) {
-        /* Cycle the color and alpha, if desired */
-        if (cycle_color) {
-            current_color += cycle_direction;
-            if (current_color < 0) {
-                current_color = 0;
-                cycle_direction = -cycle_direction;
-            }
-            if (current_color > 255) {
-                current_color = 255;
-                cycle_direction = -cycle_direction;
-            }
-        }
-        if (cycle_alpha) {
-            current_alpha += cycle_direction;
-            if (current_alpha < 0) {
-                current_alpha = 0;
-                cycle_direction = -cycle_direction;
-            }
-            if (current_alpha > 255) {
-                current_alpha = 255;
-                cycle_direction = -cycle_direction;
-            }
-        }
-        SDL_SetRenderDrawColor(renderer, 255, (Uint8) current_color,
-                               (Uint8) current_color, (Uint8) current_alpha);
-
-        if (i == 0) {
-            SDL_RenderDrawLine(renderer, 0, 0, viewport.w - 1, viewport.h - 1);
-            SDL_RenderDrawLine(renderer, 0, viewport.h - 1, viewport.w - 1, 0);
-            SDL_RenderDrawLine(renderer, 0, viewport.h / 2, viewport.w - 1, viewport.h / 2);
-            SDL_RenderDrawLine(renderer, viewport.w / 2, 0, viewport.w / 2, viewport.h - 1);
-        } else {
-            x1 = (rand() % (viewport.w*2)) - viewport.w;
-            x2 = (rand() % (viewport.w*2)) - viewport.w;
-            y1 = (rand() % (viewport.h*2)) - viewport.h;
-            y2 = (rand() % (viewport.h*2)) - viewport.h;
-            SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
-        }
-    }
-}
-
-void
-DrawRects(SDL_Renderer * renderer)
-{
-    int i;
-    SDL_Rect rect;
-    SDL_Rect viewport;
-
-    /* Query the sizes */
-    SDL_RenderGetViewport(renderer, &viewport);
-
-    for (i = 0; i < num_objects / 4; ++i) {
-        /* Cycle the color and alpha, if desired */
-        if (cycle_color) {
-            current_color += cycle_direction;
-            if (current_color < 0) {
-                current_color = 0;
-                cycle_direction = -cycle_direction;
-            }
-            if (current_color > 255) {
-                current_color = 255;
-                cycle_direction = -cycle_direction;
-            }
-        }
-        if (cycle_alpha) {
-            current_alpha += cycle_direction;
-            if (current_alpha < 0) {
-                current_alpha = 0;
-                cycle_direction = -cycle_direction;
-            }
-            if (current_alpha > 255) {
-                current_alpha = 255;
-                cycle_direction = -cycle_direction;
-            }
-        }
-        SDL_SetRenderDrawColor(renderer, 255, (Uint8) current_color,
-                               (Uint8) current_color, (Uint8) current_alpha);
-
-        rect.w = rand() % (viewport.h / 2);
-        rect.h = rand() % (viewport.h / 2);
-        rect.x = (rand() % (viewport.w*2) - viewport.w) - (rect.w / 2);
-        rect.y = (rand() % (viewport.h*2) - viewport.h) - (rect.h / 2);
-        SDL_RenderFillRect(renderer, &rect);
-    }
-}
-
-void
-loop()
-{
-    int i;
-    SDL_Event event;
-
-    /* Check for events */
-    while (SDL_PollEvent(&event)) {
-        SDLTest_CommonEvent(state, &event, &done);
-    }
-    for (i = 0; i < state->num_windows; ++i) {
-        SDL_Renderer *renderer = state->renderers[i];
-        if (state->windows[i] == NULL)
-            continue;
-        SDL_SetRenderDrawColor(renderer, 0xA0, 0xA0, 0xA0, 0xFF);
-        SDL_RenderClear(renderer);
-
-        DrawRects(renderer);
-        DrawLines(renderer);
-        DrawPoints(renderer);
-
-        SDL_RenderPresent(renderer);
-    }
-#ifdef __EMSCRIPTEN__
-    if (done) {
-        emscripten_cancel_main_loop();
+#if (defined(__MACOSX__) && __MACOSX__)
+    if (argc > 1)
+    {
+        //        setRunningPath(argv[1]);
+        setScriptDir(argv[1]);
     }
 #endif
-}
-
-int
-main(int argc, char *argv[])
-{
-    int i;
-    Uint32 then, now, frames;
-
-    /* Enable standard application logging */
-    SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
-
-    /* Initialize parameters */
-    num_objects = NUM_OBJECTS;
-
-    /* Initialize test framework */
-    state = SDLTest_CommonCreateState(argv, SDL_INIT_VIDEO);
-    if (!state) {
+    
+    /* initialize SDL */
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0)
+    {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
         return 1;
     }
-    for (i = 1; i < argc;) {
-        int consumed;
-
-        consumed = SDLTest_CommonArg(state, i);
-        if (consumed == 0) {
-            consumed = -1;
-            if (SDL_strcasecmp(argv[i], "--blend") == 0) {
-                if (argv[i + 1]) {
-                    if (SDL_strcasecmp(argv[i + 1], "none") == 0) {
-                        blendMode = SDL_BLENDMODE_NONE;
-                        consumed = 2;
-                    } else if (SDL_strcasecmp(argv[i + 1], "blend") == 0) {
-                        blendMode = SDL_BLENDMODE_BLEND;
-                        consumed = 2;
-                    } else if (SDL_strcasecmp(argv[i + 1], "add") == 0) {
-                        blendMode = SDL_BLENDMODE_ADD;
-                        consumed = 2;
-                    } else if (SDL_strcasecmp(argv[i + 1], "mod") == 0) {
-                        blendMode = SDL_BLENDMODE_MOD;
-                        consumed = 2;
-                    }
-                }
-            } else if (SDL_strcasecmp(argv[i], "--cyclecolor") == 0) {
-                cycle_color = SDL_TRUE;
-                consumed = 1;
-            } else if (SDL_strcasecmp(argv[i], "--cyclealpha") == 0) {
-                cycle_alpha = SDL_TRUE;
-                consumed = 1;
-            } else if (SDL_isdigit(*argv[i])) {
-                num_objects = SDL_atoi(argv[i]);
-                consumed = 1;
-            }
-        }
-        if (consumed < 0) {
-            SDL_Log("Usage: %s %s [--blend none|blend|add|mod] [--cyclecolor] [--cyclealpha]\n",
-                    argv[0], SDLTest_CommonUsage(state));
-            return 1;
-        }
-        i += consumed;
+    
+    // Check for joysticks
+    if (SDL_NumJoysticks() < 1)
+    {
+        printf("Warning: No joysticks connected!\n");
     }
-    if (!SDLTest_CommonInit(state)) {
-        return 2;
-    }
-
-    /* Create the windows and initialize the renderers */
-    for (i = 0; i < state->num_windows; ++i) {
-        SDL_Renderer *renderer = state->renderers[i];
-        SDL_SetRenderDrawBlendMode(renderer, blendMode);
-        SDL_SetRenderDrawColor(renderer, 0xA0, 0xA0, 0xA0, 0xFF);
-        SDL_RenderClear(renderer);
-    }
-
-    srand((unsigned int)time(NULL));
-
-    /* Main render loop */
-    frames = 0;
-    then = SDL_GetTicks();
-    done = 0;
-
-#ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop(loop, 0, 1);
-#else
-    while (!done) {
-        ++frames;
-        loop();
-        }
+    
+#if !defined(__ANDROID__)
+    SDL_GetDesktopDisplayMode(0, &gDisplayMode);
 #endif
-
-
-    SDLTest_CommonQuit(state);
-
-    /* Print out some timing information */
-    now = SDL_GetTicks();
-    if (now > then) {
-        double fps = ((double) frames * 1000) / (now - then);
-        SDL_Log("%2.2f frames per second\n", fps);
+    
+#if defined(__EMSCRIPTEN__)
+    gDisplayMode.h = 725.0f;
+    float div = gDisplayMode.h / 9.0;
+    gDisplayMode.w = div * 16.0f;
+    //      gDisplayMode.h = 600.0f;
+#endif
+    
+#if defined(__EMSCRIPTEN__) || defined(__ANDROID__) || defined(__IPHONEOS__)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+#endif
+    
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    
+#if defined(__MACOSX__)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_EventState(SDL_DROPFILE, SDL_ENABLE);
+#endif
+    
+    /* create window and renderer */
+    gWindow =
+    SDL_CreateWindow("NJLIGameEngine", SDL_WINDOWPOS_CENTERED,
+                     SDL_WINDOWPOS_CENTERED, gDisplayMode.w, gDisplayMode.h,
+                     SDL_WINDOW_OPENGL
+#if defined(__MACOSX__)
+                     | SDL_WINDOW_MAXIMIZED | SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_UTILITY
+#elif !defined(__EMSCRIPTEN__)
+                     | SDL_WINDOW_FULLSCREEN
+#endif
+                     | SDL_WINDOW_RESIZABLE
+#if !defined(__EMSCRIPTEN__)
+                     | SDL_WINDOW_ALLOW_HIGHDPI
+#endif
+                     );
+    
+#if defined(__MACOSX__)
+    createRenderer();
+#endif
+    
+    // SDL_SetWindowSize(gWindow, 100, 100);
+    
+    int w, h;
+    SDL_GetWindowSize(gWindow, &w, &h);
+    SDL_Log("SDL_GetWindowSize #%d: current display mode is %dx%dpx", 0, w, h);
+    
+    //#if defined(__MACOSX__)
+    //#if !(defined(NDEBUG))
+    //        SDL_SetWindowSize(gWindow, w * 0.25f, h * 0.25f);
+    //#endif
+    //#endif
+    
+    SDL_GL_GetDrawableSize(gWindow, &w, &h);
+    SDL_Log("SDL_GL_GetDrawableSize #%d: current display mode is %dx%dpx", 0, w,
+            h);
+    
+    if (!gWindow)
+    {
+        printf("Could not initialize Window\n");
+        return 1;
     }
+    
+#if (defined(__ANDROID__) && (__ANDROID__))
+    SDL_SetWindowFullscreen(gWindow, SDL_TRUE);
+#endif
+    
+    gGlContext = SDL_GL_CreateContext(gWindow);
+    if (!njli::NJLIGameEngine::create(
+                                      DeviceUtil::hardwareDescription().c_str()))
+    {
+        cerr << "Error initializing OpenGL" << endl;
+        return 1;
+    }
+    
+    gGraphics = unique_ptr<Graphics>(new Graphics(gWindow));
+    
+#if (defined(__IPHONEOS__) && __IPHONEOS__)
+    SDL_AddEventWatch(EventFilter, NULL);
+#endif
+    
+    int drawableW, drawableH;
+    int screen_w, screen_h;
+    float pointSizeScale;
+    
+    /* The window size and drawable size may be different when highdpi is
+     * enabled,
+     * due to the increased pixel density of the drawable. */
+    SDL_GetWindowSize(gWindow, &screen_w, &screen_h);
+    //    SDL_GL_GetDrawableSize(gWindow, &drawableW, &drawableH);
+    drawableW = gDisplayMode.w;
+    drawableH = gDisplayMode.h;
+    
+    /* In OpenGL, point sizes are always in pixels. We don't want them looking
+     * tiny on a retina screen. */
+    pointSizeScale = (float)drawableH / (float)screen_h;
+    
+    //#if defined(__MACOSX__)
+    //    SDL_GetWindowSize(gWindow, &w, &h);
+    //#else
+    //    SDL_GL_GetDrawableSize(gWindow, &w, &h);
+    //#endif
+    //#if defined(__EMSCRIPTEN__)
+    NJLI_HandleResize(drawableW, drawableH, gDisplayMode.format,
+                      gDisplayMode.refresh_rate);
+    //#endif
+    
+    gDone = (njli::NJLIGameEngine::start(argc, argv) == false) ? 1 : 0;
+    
+#if defined(__EMSCRIPTEN__)
+    emscripten_set_main_loop(mainloop, 0, 0);
+#else
+    
+    while (!gDone)
+    {
+#if defined(__IPHONEOS__) && __IPHONEOS__
+        handleInput();
+#else
+        mainloop();
+#endif
+    }
+    
+    NJLI_HandleSurfaceDestroyed();
+    
+#endif
+    
     return 0;
 }
-
-/* vi: set ts=4 sw=4 expandtab: */
-
